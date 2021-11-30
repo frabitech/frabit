e372a22a19e449f093fc54a4b028c559779e854c
blylei <blylei.info@gmail.com>
2021-11-30 23:54:12 +0800

    add rpc_client

--- frabit/cli_rpc.py
+++ frabit/cli_rpc.py
@@ -7,19 +7,7 @@ import grpc
 import logging
 
 def run():
-    # NOTE(gRPC Python Team): .close() is possible on a channel and should be
-    # used in circumstances in which the with statement does not fit the needs
-    # of the code.
-    with grpc.insecure_channel('localhost:50051') as channel:
-        stub = route_guide_pb2_grpc.RouteGuideStub(channel)
-        print("-------------- GetFeature --------------")
-        guide_get_feature(stub)
-        print("-------------- ListFeatures --------------")
-        guide_list_features(stub)
-        print("-------------- RecordRoute --------------")
-        guide_record_route(stub)
-        print("-------------- RouteChat --------------")
-        guide_route_chat(stub)
+    pass
 
 
 if __name__ == '__main__':
